6e4e7e7d8807ad1289101edbd96ca33036c86bc8
Daria-V1985 <69900770+Daria-V1985@users.noreply.github.com>
2022-12-28 23:48:48 +0300

    Исправление недочетов
    Исправление кода в файлах css и js

--- js/main.js
+++ js/main.js
@@ -9,19 +9,24 @@ $(function(){
   $('.tab').on('click', function(e){
     e.preventDefault();
       
-    $('.tab').removeClass('tab--active');
-    $('.tabs-content').removeClass('tabs-content--active');
+    $($(this).siblings()).removeClass('tab--active');
+    $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
     $(this).addClass('tab--active');
     $($(this).attr('href')).addClass('tabs-content--active');
   });
 
   $('.product-item__favorite').on('click', function(){
-    $('.product-item__favorite').toggleClass('product-item__favorite--active')
+    $(this).toggleClass('product-item__favorite--active')
   });
 
-  $('.product__slider').slick({
-    
+  $('.product-slider').slick({
+    dots:false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    touchMove: false,
+    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black_left.svg" alt=""></button>',
+    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black_right.svg" alt=""></button>',
   });
 
 });
